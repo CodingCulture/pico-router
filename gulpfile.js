96b2503f1bdbd9629e2a994d5d41ecefb30e05d0
Niels Vermaut <nielsvermaut@gmail.com>
2016-02-04 12:11:20 +0100

    Added gulp workflow to build for npm

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,12 @@
 var gulp = require('gulp');
-var util = require('gulp-util');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var runSequence = require('run-sequence');
 var path = require('path');
 var babel = require('gulp-babel');
-var browserify = require('browserify');
-var babelify = require('babelify');
-var reactify = require('reactify');
-var source = require('vinyl-source-stream');
 
 /**
  * Sets the defaults Gulp Task.
  */
 gulp.task('default', function(){
-    runSequence('build');
-});
-
-/**
- * Builds all assets.
- */
-gulp.task('build', function(){
-    runSequence('concatReact', function(){});
+   console.log('Run gulp build');
 });
 
 /**
@@ -29,24 +14,9 @@ gulp.task('build', function(){
  * This file will be written to web/dist/js/napoleon-react.js
  */
 gulp.task('build', function(){
-    return gulp.src('src/ComponentSpawner.react.js')
+    return gulp.src('src/router.react.js')
         .pipe(babel({
             presets: ['es2015','stage-0','react']
         }))
         .pipe(gulp.dest('dist'));
-});
-
-gulp.task('buildTest', function(){
-    var bundlerTestBed = browserify({
-        entries: ["example/js/src/bootstrap.js"],
-        debug: true,
-        cache: {}, packageCache: {}, fullPaths: true
-    });
-
-    return bundlerTestBed
-        .transform(babelify, {presets: ['es2015', 'react', 'stage-0'], plugins: ['add-module-exports']})
-        .bundle()
-        .pipe(source('ComponentFactory.js'))
-        .pipe(gulp.dest('example/js/dist/'))
-        ;
 });
